ac63fc7dc5619f995148ef5e5e4a6e46b64a05cc
Bill Gale <bill.gale@krystal.uk>
2022-12-06 12:43:28 +0000

    feat: add repo label to image and defaults to vars

--- dockerfile
+++ dockerfile
@@ -1,5 +1,7 @@
 FROM ubuntu:latest
 
+LABEL org.opencontainers.image.source=https://github.com/ikadix/static_ssh
+
 RUN apt update && apt install  openssh-server sudo rsync -y
 
 RUN useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HOSTING_PATH="${HOSTING_PATH:-/app}"
+PORT="${PORT:-8080}"
+
 # Run the ssh daemon
 /usr/sbin/sshd
 
@@ -7,4 +10,4 @@
 cd "$HOSTING_PATH" || exit
 
 # Run a http server in the hosting path on port 8080
-python3 -m http.server $PORT
+python3 -m http.server "$PORT"
